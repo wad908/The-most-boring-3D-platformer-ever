c4a6a5745947a724db7c56651b9febf8e2e2b3cc
Sadiq645 <23328@students.mrgs.school.nz>
2026-05-07 10:49:43 +1200

    camera
    Adding camera controls

--- the-most-boring-3d-platformer-ever/player.gd
+++ the-most-boring-3d-platformer-ever/player.gd
@@ -3,11 +3,14 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	Input. set_mouse_mode(Input. MOUSE_MODE_CAPTURED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var input = Input.get_action_strength("ui_up")
-	apply_central_impulse(input * Vector3.FORWARD * 1200.0 * delta)
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward","move_back")
 	
+	apply_central_force(input * 1200.0 * delta)
+	if Input. is_action_just_pressed("ui_cancel"):
+		Input. set_mouse_mode(Input. MOUSE_MODE_VISIBLE)
